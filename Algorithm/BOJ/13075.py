2885a9d5f828ea25ddc5879efc6a56bf1be48f99
hyenzzang <2017100898@khu.ac.kr>
2023-05-02 21:47:08 +0900

    +

---
+++ Algorithm/BOJ/13075.py
@@ -0,0 +1,36 @@
+T = int(input())
+for i in range(0, T):
+    N = int(input())
+    x = [[1, 1], [1, 0]]
+
+    def mul(A, B):
+        N = len(A)
+        c = [[0] * N for _ in range(N)]
+        # 행 순회
+        for i in range(N):
+            # 열 순회
+            for j in range(N):
+                for k in range(N):
+                    c[i][j] += A[i][k] * B[k][j] % 10**9
+
+        return c
+
+    # 분할정복을 위해 사용하는 함수
+    def power(A, N):
+        if N == 0 :
+            return [[0, 0], [0,0]]
+        
+        elif N == 1:
+            return A
+        
+        a = power(A, N // 2)
+
+        if N % 2 == 0:
+            return mul(a , a)
+        
+        else:
+            return mul(mul(a, a), A)
+        
+    result = power(x, N)
+
+    print(result[0][1] %  10**9)
